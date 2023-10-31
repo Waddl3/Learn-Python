b59d104b9c89333d748d5c45d9152b5b3d8bdf68
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2023-10-31 15:40:54 -0700

    day_1 complete

---
+++ day_1/main.py
@@ -0,0 +1,16 @@
+# Name Generator
+
+#1. Create a greeting
+print("Welcome to Name Generator")
+
+# 2. Ask user for city 
+city = input("What is the city you were born in?\n")
+
+# 3. Ask user for favorite food/lucky number
+favorite = input("What is your favorite food/number?\n")
+
+# 4. Combine the two and display the result
+print("Genrated:")
+print(city + " " + favorite)
+
+# 5. make sure input cursor shows on new Line
