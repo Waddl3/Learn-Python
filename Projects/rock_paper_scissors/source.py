ab8ac37edb8360870d12391b2942296d2044b6dd
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2023-11-07 20:22:11 -0800

    Rock, Paper, Scissors game in Python

--- Projects/rock_paper_scissors/source.py
+++ Projects/rock_paper_scissors/source.py
@@ -1,26 +1,39 @@
 import rps_mod
 import random
 
-user = input("What do you choose? Type: 0 = Rock, 1 = Paper, 2 = Scissors\n")
+user = int(input("What do you choose? Type: 0 = Rock, 1 = Paper, 2 = Scissors\n"))
 r = random.randint(0, 2)
-rock = rps_mod.rock
-paper = rps_mod.paper
-scissors = rps_mod.scissors
-game = [rock, paper, scissors]
+game = [rps_mod.rock, rps_mod.paper, rps_mod.scissors]
 
 # player v. computer chose
 print("You chose:\n")
-print(game[user]())  # Invoke the function to print the choice
+print(game[user])  # Invoke the function to print the choice
 print("computer chose:\n")
-print(game[r]())  # Invoke the function to print the choice
+print(game[r])  # Invoke the function to print the choice
 
 # rock
-if game[r]() == game[user]():
-    print(rps_mod.tie())
-if game[user] == rps_mod.paper:
-    print("You Lose")
-if game[user] == rps_mod.scissors:
-    print("You win")
+if game[r] == rps_mod.rock:
+    if game[r] == game[user]:
+        print(rps_mod.tie)
+    if game[user] == rps_mod.paper:
+            print("You win")
+    if game[user] == rps_mod.scissors:
+            print("You lose")
     
 # paper
+if game[r] == rps_mod.paper:
+    if game[r] == game[user]:
+        print(rps_mod.tie)
+    if game[user] == rps_mod.rock:
+            print("You Lose")
+    if game[user] == rps_mod.scissors:
+            print("You win")
+    
 # scissors
+if game[r] == rps_mod.scissors:
+    if game[r] == game[user]:
+        print(rps_mod.tie)
+    if game[user] == rps_mod.paper:
+            print("You Lose")
+    if game[user] == rps_mod.rock:
+            print("You win")
