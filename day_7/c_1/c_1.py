2ca51786c6c09d53515682b166cfebd526b91b1a
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2023-11-11 20:25:35 -0800

    step 3 complete on to step 4

--- day_7/c_1/c_1.py
+++ day_7/c_1/c_1.py
@@ -1,29 +1,34 @@
 #Step 1 
 import random
+import hangman_fig
 
 word_list = ["aardvark", "baboon", "camel"]
 display = []
 chosen_word = random.choice(word_list)
-index = 0
+word_size = len(chosen_word)
+end_game = False
 
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-for _ in range(len(chosen_word)):
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+#Set 'lives' to equal 6.
+
+for _ in range(word_size):
     display.append("_")
 
-guess = input("Guess a letter: ").lower()
 
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
 
-for char in chosen_word:
-    if char == guess:
-        display[index] = guess
-    
-    index += 1
-        
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-print(display)
+while not end_game:
+    guess = input("Guess a letter: ").lower()
+
+    for pos in range(word_size):
+        char = chosen_word[pos]
+        if char == guess:
+            display[pos] = guess
+            
+        #   TODO-2: - If guess is not a letter in the chosen_word,
+        #   Then reduce 'lives' by 1. 
+        #   If lives goes down to 0 then the game should stop and it should print "You lose."
+
+    print(display)
+    if "_" not in display:
+        end_game = True
+        print("You WIN!!")
