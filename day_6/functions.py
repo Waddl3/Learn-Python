0546b42f141f3500fbf2655c563019399e8c0ad0
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2023-11-11 18:54:09 -0800

    Day 6 complete

---
+++ day_6/functions.py
@@ -0,0 +1,13 @@
+def my_function():
+    print("Hello")
+    print("Bye")
+    #   Do this
+    #   Then do this
+    #   Finally do this
+    
+my_function()
+
+i = 0
+while i < 10:
+    print(i)
+    i += 1
