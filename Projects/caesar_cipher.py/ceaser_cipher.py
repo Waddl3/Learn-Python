637edc1772df1c0f7803dd1af6637c6c1c528ebf
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2023-11-21 22:21:59 -0800

    Completed Ceaser Cipher

--- Projects/caesar_cipher.py/ceaser_cipher.py
+++ Projects/caesar_cipher.py/ceaser_cipher.py
@@ -1,27 +1,4 @@
 from alphabet import alphabet
-
-# def encrypt(text, shift):
-#     encrypted_text = ""
-#     size = len(alphabet)
-#     for c in text:
-#         index = alphabet.index(c)
-#         new_index = shift + index
-#         if new_index >= size:
-#             new_index %= size
-#         encrypted_text += alphabet[new_index]
-    
-#     print(f"The encoded text is {encrypted_text}")
-        
-# def decode(text, shift):
-#     decoded_txt = ""
-#     for c in text:
-#         index = alphabet.index(c)
-#         new_index = index - shift
-#         decoded_txt += alphabet[new_index]
-        
-#     print(f"The encoded text is {decoded_txt}")
-
-#   Put everything into one single method.
     
 def ceaser(text, shift, direction):
     ceaser_txt = ""
@@ -31,17 +8,12 @@ def ceaser(text, shift, direction):
             shift *= -1
     
     for c in text:
-        if c == " " or c == ";":
-            if c == " ":
-                c 
-            
-        index = alphabet.index(c)
-        new_index = index + shift
-        if new_index >= size: new_index %= size
-        ceaser_txt += alphabet[new_index]
+        if c  in alphabet:
+            index = alphabet.index(c)
+            new_index = index + shift
+            if new_index >= size: new_index %= size
+            ceaser_txt += alphabet[new_index]
+        else:
+            ceaser_txt += c
 
     print(f"The {direction} text is {ceaser_txt}")
-    
-    # TO-DO: What is user enters a number/symbol/space?
-    # TO-DO: Restart feature?
-    # TO-DO: What is user enters shidt greater than 26
--- Projects/caesar_cipher.py/main.py
+++ Projects/caesar_cipher.py/main.py
@@ -4,10 +4,17 @@ from cc_logo import logo
 #   Logo
 print(logo)
 
-#   User text choice
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+active = True
 
-#   Encode or Decode
-ceaser(text, shift, direction)
+while active:
+    #   User text choice
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    #   Encode or Decode
+    ceaser(text, shift, direction)
+    
+    choice = input("End? 'n'; Otherwise: Press any key \n").lower()
+    print("\n")
+    if choice == "n": active = False
