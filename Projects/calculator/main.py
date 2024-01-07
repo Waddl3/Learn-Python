e660fc54b759e0d606728f59fe235fa33c5ecdfa
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2024-01-07 00:40:51 -0800

    Updated Calculator

--- Projects/calculator/main.py
+++ Projects/calculator/main.py
@@ -16,29 +16,31 @@ n = int(input("What's the first numbers?: "))
 m = int(input("What's the second numbers?: "))
 s = operations[input("Choose a symbol: ")]
 t = s(n, m)
+_continue = True
 print(t)
     
-while True:
-    next_input = input("Enter the next number (or 'q' to quit): ")
+while _continue:
+    next_input = input("Enter the next number (or 'n' to quit): ")
     
-    if next_input.lower() == 'q':
-        break
+    if next_input.lower() == 'n':
+        _continue = False
+    else:
+        if not next_input.isdigit():
+            print("Invalid input. Enter a digit or 'q' to quit.")
+            continue
+        
+        next_number = int(next_input)
+        print(symbol)
+        
+        selected_symbol = input("Symbol: ")
+        
+        if selected_symbol not in operations:
+            print("Invalid operation symbol selected.")
+            continue
+        
+        operation = operations[selected_symbol]
+        t = operation(t, next_number)
+        print(t)
     
-    if not next_input.isdigit():
-        print("Invalid input. Enter a digit or 'q' to quit.")
-        continue
-    
-    next_number = int(next_input)
-    print(symbol)
-    
-    selected_symbol = input("Symbol: ")
-    
-    if selected_symbol not in operations:
-        print("Invalid operation symbol selected.")
-        continue
-    
-    operation = operations[selected_symbol]
-    t = operation(t, next_number)
-    print(t)
 
 
